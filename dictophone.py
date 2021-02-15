e9f48d8ab8cac62f06f2bbe6925e9ad56530ec1d
tagirium <50774982+tagirium@users.noreply.github.com>
2021-02-15 19:26:32 +0300

    Update dictophone.py
    Now done with pyaudio and threads

--- dictophone.py
+++ dictophone.py
@@ -1,9 +1,48 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
+import pyaudio
+import wave
+import threading
 
-fs = 44100  # Sample rate
-seconds = 3  # Duration of recording
 
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()  # Wait until recording is finished
-write('output.wav', fs, myrecording)  # Save as WAV file
+class Dictophone:
+	chunk = 1024  # Record in chunks of 1024 samples
+	sample_format = pyaudio.paInt16  # 16 bits per sample
+	channels = 2
+	fs = 44100  # Record at 44100 samples per second
+	seconds = 3
+	filename = "output.wav"
+
+	def __init__(self):
+		self.p = pyaudio.PyAudio()  # Create an interface to PortAudio
+		self.stream = self.p.open(format=self.sample_format,
+							 channels=self.channels,
+							 rate=self.fs,
+							 frames_per_buffer=self.chunk,
+							 input=True)
+		self.frames = []  # Initialize array to store frames
+		self.flag = True
+
+	def startrecording(self):
+		self.p = pyaudio.PyAudio()
+		self.stream = self.p.open(format=self.sample_format, channels=self.channels, rate=self.fs,
+								  frames_per_buffer=self.chunk, input=True)
+		self.isrecording = True
+
+		print('Recording')
+		t = threading.Thread(target=self.record)
+		t.start()
+
+	def stoprecording(self):
+		self.isrecording = False
+		print('recording complete')
+		self.filename = "output.wav"
+		wf = wave.open(self.filename, 'wb')
+		wf.setnchannels(self.channels)
+		wf.setsampwidth(self.p.get_sample_size(self.sample_format))
+		wf.setframerate(self.fs)
+		wf.writeframes(b''.join(self.frames))
+		wf.close()
+
+	def record(self):
+		while self.isrecording:
+			data = self.stream.read(self.chunk)
+			self.frames.append(data)
